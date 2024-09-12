06b75bd9ec14dd229e31065bf1e10efecca28ff5
mohammad <mohammaddvlpr@gmail.com>
2024-09-12 17:20:41 +0330

    removed progress bar from activity_main.xml implemented todo in MainActivity.kt

--- app/src/main/java/com/jabama/challenge/ui/main/MainActivity.kt
+++ app/src/main/java/com/jabama/challenge/ui/main/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.jabama.challenge.ui.main
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.jabama.challenge.login.R
 
@@ -9,16 +12,18 @@ const val CLIENT_SECRET = "bbb212a4078de9c927ec8cae049b85c15ad32fab"
 const val REDIRECT_URI = "oauth://jabamatest.com"
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var authorize: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        todo:handle init of authorize
-//        authorize.setOnClickListener { view ->
-//            val url = "https://github.com/login/oauth/authorize?client_id=$CLIENT_ID&redirect_uri=$REDIRECT_URI&scope=repo user&state=0"
-//            val i = Intent(Intent.ACTION_VIEW)
-//            i.data = Uri.parse(url)
-//            startActivity(i)
-//        }
+        authorize = findViewById(R.id.authorize)
+        authorize.setOnClickListener {
+            val url =
+                "https://github.com/login/oauth/authorize?client_id=$CLIENT_ID&redirect_uri=$REDIRECT_URI&scope=repo user&state=0"
+            val i = Intent(Intent.ACTION_VIEW)
+            i.data = Uri.parse(url)
+            startActivity(i)
+        }
     }
 }
