1cb7ae5d07c949a7a06173b5dbc0009d640856d7
mohammad <mohammaddvlpr@gmail.com>
2024-09-12 23:19:52 +0330

    implemented SearchRemoteRemoteDataSourceImpl refactored SearchPagingSource from any to SearchModel

--- app/src/main/java/com/jabama/challenge/repository/search/dataSource/remote/SearchRemoteRemoteDataSourceImpl.kt
+++ app/src/main/java/com/jabama/challenge/repository/search/dataSource/remote/SearchRemoteRemoteDataSourceImpl.kt
@@ -1,11 +1,21 @@
 package com.jabama.challenge.repository.search.dataSource.remote
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.jabama.challenge.domain.usecase.search.model.SearchModel
+import com.jabama.challenge.network.search.SearchService
+import com.jabama.challenge.repository.search.paging.SearchPagingSource
 import kotlinx.coroutines.flow.Flow
 
-class SearchRemoteRemoteDataSourceImpl : SearchRemoteDataSource {
+class SearchRemoteRemoteDataSourceImpl(private val searchService: SearchService) : SearchRemoteDataSource {
     override fun getSearchFlow(query: String): Flow<PagingData<SearchModel>> {
-        TODO("Not yet implemented")
+        return Pager(
+            config = PagingConfig(pageSize = 15, prefetchDistance = 2),
+            pagingSourceFactory = {
+                SearchPagingSource(query = query , searchService = searchService)
+            }
+        ).flow
+
     }
 }
--- app/src/main/java/com/jabama/challenge/repository/search/paging/SearchPagingSource.kt
+++ app/src/main/java/com/jabama/challenge/repository/search/paging/SearchPagingSource.kt
@@ -2,21 +2,22 @@ package com.jabama.challenge.repository.search.paging
 
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
+import com.jabama.challenge.domain.usecase.search.model.SearchModel
 import com.jabama.challenge.network.search.SearchService
 import com.jabama.challenge.repository.apiCall
 
 class SearchPagingSource(
     private val query: String,
     private val searchService: SearchService,
-) : PagingSource<Int, Any>() {
-    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Any> {
+) : PagingSource<Int, SearchModel>() {
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, SearchModel> {
         val key = params.key ?: 0
         val result = apiCall { searchService.search(query = query) }
 
         TODO()
     }
 
-    override fun getRefreshKey(state: PagingState<Int, Any>): Int? {
+    override fun getRefreshKey(state: PagingState<Int, SearchModel>): Int? {
         return state.anchorPosition?.let { anchorPosition ->
             val anchorPage = state.closestPageToPosition(anchorPosition)
             anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
