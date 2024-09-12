91047bd735c586e79dc68cd409acc3289cdb24cb
mohammad <mohammaddvlpr@gmail.com>
2024-09-13 01:50:40 +0330

    added coil for loading image from internet removed todo

--- app/src/main/java/com/jabama/challenge/ui/search/SearchActivity.kt
+++ app/src/main/java/com/jabama/challenge/ui/search/SearchActivity.kt
@@ -3,7 +3,6 @@ package com.jabama.challenge.ui.search
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
@@ -16,13 +15,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.jabama.challenge.domain.usecase.search.model.SearchModel
 import com.jabama.challenge.login.R
 import com.jabama.challenge.ui.HyperLinkText
@@ -85,10 +84,9 @@ fun SearchItem(
 
 
         }
-//        todo:replace with async image
-        Image(
+        AsyncImage(
+            model = searchModel.avatarUrl,
             modifier = Modifier.clip(RoundedCornerShape(8.dp)).aspectRatio(1f).fillMaxHeight(),
-            painter = painterResource(0),
             contentDescription = stringResource(R.string.repository_image)
         )
 
