d166a7cdd5a5b98ae4f36639be25858e4c6a685c
mohammad <mohammaddvlpr@gmail.com>
2024-09-12 17:57:36 +0330

    added di for GetIsLoginUseCase

--- app/src/main/java/com/jabama/challenge/network/di/UseCaseModule.kt
+++ app/src/main/java/com/jabama/challenge/network/di/UseCaseModule.kt
@@ -1,8 +1,10 @@
 package com.jabama.challenge.network.di
 
+import com.jabama.challenge.domain.usecase.accessToken.GetIsLoginUseCase
 import com.jabama.challenge.domain.usecase.login.GetAccessTokenUseCase
 import org.koin.dsl.module
 
 val useCaseModule = module {
     factory<GetAccessTokenUseCase> { GetAccessTokenUseCase(get(), get()) }
+    factory<GetIsLoginUseCase> { GetIsLoginUseCase(get()) }
 }
