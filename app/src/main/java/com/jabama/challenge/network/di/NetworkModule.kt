23bce4f487850282b35d4b578639be5650607ddd
mohammad <mohammaddvlpr@gmail.com>
2024-09-13 14:46:22 +0330

    added interceptor for authentication fixed di issues

--- app/src/main/java/com/jabama/challenge/network/di/NetworkModule.kt
+++ app/src/main/java/com/jabama/challenge/network/di/NetworkModule.kt
@@ -1,5 +1,6 @@
 package com.jabama.challenge.network.di
 
+import com.jabama.challenge.network.AuthInterceptor
 import com.jabama.challenge.repository.search.SearchRepository
 import com.jabama.challenge.repository.search.SearchRepositoryImpl
 import com.jabama.challenge.repository.token.TokenRepository
@@ -19,24 +20,32 @@ const val RETROFIT = "RETROFIT"
 const val READ_TIMEOUT = "READ_TIMEOUT"
 const val WRITE_TIMEOUT = "WRITE_TIMEOUT"
 const val CONNECTION_TIMEOUT = "CONNECTION_TIMEOUT"
+const val LOGGING_INTERCEPTOR = "LOGGING_INTERCEPTOR"
+const val AUTH_INTERCEPTOR = "AUTH_INTERCEPTOR"
 val networkModule = module {
 
     single<Long>(named(READ_TIMEOUT)) { 30 * 1000 }
     single<Long>(named(WRITE_TIMEOUT)) { 10 * 1000 }
     single<Long>(named(CONNECTION_TIMEOUT)) { 10 * 1000 }
 
-    factory<Interceptor> {
+    factory<Interceptor>(named(LOGGING_INTERCEPTOR)) {
         HttpLoggingInterceptor()
             .setLevel(HttpLoggingInterceptor.Level.HEADERS)
             .setLevel(HttpLoggingInterceptor.Level.BODY)
     }
+
+    factory<Interceptor>(named(AUTH_INTERCEPTOR)) {
+        AuthInterceptor(get())
+    }
+
 
     factory(named(OK_HTTP)) {
         OkHttpClient.Builder()
             .readTimeout(get(named(READ_TIMEOUT)), TimeUnit.MILLISECONDS)
             .writeTimeout(get(named(WRITE_TIMEOUT)), TimeUnit.MILLISECONDS)
             .connectTimeout(get(named(CONNECTION_TIMEOUT)), TimeUnit.MILLISECONDS)
-            .addInterceptor(get())
+            .addInterceptor(get<Interceptor>(named(LOGGING_INTERCEPTOR)))
+            .addInterceptor(get<Interceptor>(named(AUTH_INTERCEPTOR)))
             .build()
     }
 
