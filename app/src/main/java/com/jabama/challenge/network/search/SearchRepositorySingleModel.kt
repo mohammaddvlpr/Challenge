d3f427cda6556cd54dba7cb51891d48185afd1f6
mohammad <mohammaddvlpr@gmail.com>
2024-09-15 01:09:34 +0330

    changed url to html_url in SearchRepositorySingleModel.kt

--- app/src/main/java/com/jabama/challenge/network/search/SearchRepositorySingleModel.kt
+++ app/src/main/java/com/jabama/challenge/network/search/SearchRepositorySingleModel.kt
@@ -5,6 +5,6 @@ import com.google.gson.annotations.SerializedName
 data class SearchRepositorySingleModel(
     @SerializedName("full_name") val fullName: String,
     @SerializedName("avatar_url") val avatarUrl: String?,
-    @SerializedName("url") val url: String,
+    @SerializedName("html_url") val url: String,
     @SerializedName("private") val isPrivate: Boolean
 )
