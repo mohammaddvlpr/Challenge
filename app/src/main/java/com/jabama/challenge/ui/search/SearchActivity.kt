7c0aab82d10a1957f4b117c393aa01cc1e5dad86
mohammad <mohammaddvlpr@gmail.com>
2024-09-15 03:22:10 +0330

    added BackHandler to SearchActivity

--- app/src/main/java/com/jabama/challenge/ui/search/SearchActivity.kt
+++ app/src/main/java/com/jabama/challenge/ui/search/SearchActivity.kt
@@ -1,6 +1,7 @@
 package com.jabama.challenge.ui.search
 
 import android.os.Bundle
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Arrangement
@@ -60,6 +61,10 @@ class SearchActivity : AppCompatActivity() {
                 pagingDataFlow = searchViewModel.pagingFlow
             ) {
                 searchViewModel.onQueryChange(it)
+            }
+
+            BackHandler {
+                finish()
             }
         }
 
