f598dfff6336a36d68a588aba14117198224b337
mohammad <mohammaddvlpr@gmail.com>
2024-09-15 00:38:54 +0330

    fixed di issues

--- app/src/main/java/com/jabama/challenge/network/di/SearchModule.kt
+++ app/src/main/java/com/jabama/challenge/network/di/SearchModule.kt
@@ -1,15 +1,18 @@
 package com.jabama.challenge.network.di
 
 import com.jabama.challenge.network.search.SearchService
+import com.jabama.challenge.repository.search.SearchMapper
 import com.jabama.challenge.repository.search.dataSource.remote.SearchRemoteDataSource
 import com.jabama.challenge.repository.search.dataSource.remote.SearchRemoteRemoteDataSourceImpl
+import com.jabama.challenge.ui.search.model.UiMapper
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import retrofit2.Retrofit
 
 val searchModule = module {
-    factory {
-        factory { get<Retrofit>(named(RETROFIT)).create(SearchService::class.java) }
-        factory<SearchRemoteDataSource> { SearchRemoteRemoteDataSourceImpl(get(), get()) }
-    }
+    factory<SearchService> { get<Retrofit>(named(RETROFIT)).create(SearchService::class.java) }
+    factory<SearchRemoteDataSource> { SearchRemoteRemoteDataSourceImpl(get(), get()) }
+    factory<UiMapper> { UiMapper(get()) }
+    factory<SearchMapper> { SearchMapper() }
+
 }
