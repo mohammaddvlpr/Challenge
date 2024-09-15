b2b88a5802bc97305405d9bac0aa9a955bde7b13
mohammad <mohammaddvlpr@gmail.com>
2024-09-15 04:53:24 +0330

    created mapper for AuthRepositoryImpl

--- Data/src/main/java/com/jabama/challenge/data/repository/oauth/AuthRepositoryImpl.kt
+++ Data/src/main/java/com/jabama/challenge/data/repository/oauth/AuthRepositoryImpl.kt
@@ -1,9 +1,23 @@
 package com.jabama.challenge.data.repository.oauth
 
 import com.jabama.challenge.data.network.oauth.AuthService
+import com.jabama.challenge.data.repository.apiCall
 import com.jabama.challenge.domain.login.AuthRepository
 import com.jabama.challenge.domain.login.RequestAccessTokenDomainModel
+import com.jabama.challenge.domain.login.ResponseAccessTokenDomainModel
 
-class AuthRepositoryImpl(private val authService: AuthService) : AuthRepository {
-    override suspend fun accessToken(requestAccessTokenDomainModel: RequestAccessTokenDomainModel) = authService.accessToken(requestAccessToken)
+class AuthRepositoryImpl(
+    private val authService: AuthService,
+    private val authMapper: AuthMapper
+) : AuthRepository {
+    override suspend fun accessToken(requestAccessTokenDomainModel: RequestAccessTokenDomainModel):Result<ResponseAccessTokenDomainModel> {
+        val result =
+            apiCall { authService.accessToken(authMapper.toData(requestAccessTokenDomainModel)) }
+        val resultValue = result.getOrNull()
+
+        return if (result.isSuccess && resultValue != null )
+            Result.success(authMapper.toDomain(resultValue))
+        else
+            Result.failure(result.exceptionOrNull() ?: Exception("Not known"))
+    }
 }
